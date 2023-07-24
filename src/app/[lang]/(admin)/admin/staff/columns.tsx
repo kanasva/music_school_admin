9155638940c1db7f18bbe7421381d62d5fd6f56d
Kan Asvasena <kan.asva@gmail.com>
2023-07-24 16:20:54 +0700

    24 july

--- src/app/[lang]/(admin)/admin/staff/columns.tsx
+++ src/app/[lang]/(admin)/admin/staff/columns.tsx
@@ -5,21 +5,21 @@ import { ColumnDef } from "@tanstack/react-table"
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
 export type Payment = {
-	id: string
-	name: number
-	phone: string
+	// id: string
+	// name: number
+	// phone: string
 	email: string
 }
 
 export const columns: ColumnDef<Payment>[] = [
-	{
-		accessorKey: "name",
-		header: "Name",
-	},
-	{
-		accessorKey: "phone",
-		header: "Phone",
-	},
+	// {
+	// 	accessorKey: "name",
+	// 	header: "Name",
+	// },
+	// {
+	// 	accessorKey: "phone",
+	// 	header: "Phone",
+	// },
 	{
 		accessorKey: "email",
 		header: "Email",
